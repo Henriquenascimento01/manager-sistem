5f71fabf3ac210edc498065c5c667ca5ff6041e5
henriquenascimento01 <henriquenascimento300@hotmail.com>
2023-01-10 00:53:19 -0300

    Feito página de listagem de produtos com imagem

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\ProductRepository;
 use Illuminate\Http\Request;
 
 class SiteController extends Controller
@@ -13,7 +14,9 @@ class SiteController extends Controller
      */
     public function index()
     {
-        return view('marketplace');
+        $products = ProductRepository::all();
+
+        return view('marketplace', compact('products'));
     }
 
     /**
--- resources/views/marketplace.blade.php
+++ resources/views/marketplace.blade.php
@@ -1,29 +1,149 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manager sistem</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+    <style>
+        body {
+            min-height: 100vh;
+            padding-bottom: 100px;
+            position: relative;
+            background-color: black;
+        }
+
+        header {
+            background-color: black;
+            border-bottom: 1px solid gray;
+        }
+
+        .row {
+            margin: 0;
+        }
+
+        .container-fluid {
+            padding: 0;
+        }
+
+        .btn-primary {
+            border: 2px solid blue;
+        }
+
+        #navbar {
+            justify-content: space-between;
+            padding: 5px 0;
+        }
+
+        #navbar a {
+            color: white
+        }
+
+        #search-container {
+            background-image: url('/img/nft.jpg');
+            background-size: cover;
+            background-position: center;
+            height: 400px;
+            padding: 50px;
+            text-align: center;
+        }
+
+        #search-container h1 {
+            margin-bottom: 30px;
+            font-weight: 900;
+        }
+
+        #products-container {
+            padding: 50px;
+        }
+
+        #cards-container {
+            display: flex;
+        }
+
+        #products-container .card {
+            flex: 1 1 0;
+            max-width: 25%;
+            border-radius: 10px;
+            padding: 0;
+            margin: 5px;
+        }
+
+        #products-container img {
+            max-height: 150px;
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+            object-fit: cover;
+        }
+
+        .card .card-name {
+            font-size: 20px;
+            font-weight: bold;
+        }
+
+        footer {
+            text-align: center;
+            background-color: black;
+            padding: 30px;
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+        }
+
+        footer p {
+            margin-bottom: 0;
+            color: white
+        }
+    </style>
 </head>
 
 
-<body class="antialiased">
-    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-        @if (Route::has('login'))
-        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            @auth
-            <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-            @else
-            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Entrar</a>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="collapse navbar-collapse" id="navbar">
+                <a href="/" class="navbar-brand">
+                    <img src="" alt="">
+                </a>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="{{ route('login') }}" class="nav-link">Entrar</a>
+                    </li>
+                    <li class="nav-item">
+                        @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="nav-link">Registrar-se</a>
+                        @endif
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
 
-            @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Registrar-se</a>
-            @endif
-            @endauth
+    <div id="search-container" class="col md-12">
+        <h1>Seja bem vindo</h1>
+    </div>
+
+    <div id="products-container" class="col-md-12">
+        <div id="cards-container" class="row">
+            @foreach($products as $product)
+            <div class="card col-md-3">
+                <img src="/img/movies/{{ $product->image }}" alt="{{ $product->name }}">
+                <div class="card-body">
+                    <h5 class="card-name">{{ $product->name }}</h5>
+                    <a href="#" class="btn btn-primary">Comprar</a>
+                </div>
+            </div>
+            @endforeach
         </div>
-        @endif
+    </div>
+
+    <footer>
+        <p>Manager sistem &copy; 2023</p>
+    </footer>
 </body>
 
 </html>
